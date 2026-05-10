c246db4a19ca3b8948ca55d354030bf0390d6250
neya-mul <neyamulislam946@gmail.com>
2026-05-10 10:24:02 +0600

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className='flex justify-between p-5 border container mx-auto'>
+            <ul className='flex gap-4'>
+                <li>  <Link href='/'>Home</Link> </li>
+                <li>  <Link href='/destination'>Destination</Link> </li>
+                <li>  <Link href='/booking'>My Bookings</Link> </li>
+                <li>  <Link href='/admin'>Admin</Link> </li>
+            </ul>
+
+            <div>
+                <Image
+                    src={'/assets/Wanderlast.png'}
+                    height={150}
+                    width={150}
+                    alt='logo'
+                />
+            </div>
+
+     
+                <ul className='flex gap-4'>
+                    <li>  <Link href='/profile'>Profile</Link> </li>
+                    <li>  <Link href='/signIn'>Sign In</Link> </li>
+                    <li>  <Link href='/login'>Login</Link> </li>
+    
+                </ul>
+       
+        </nav>
+    )
+}
