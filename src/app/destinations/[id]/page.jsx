4b780d8e8953352c82197f585a02ef9b0a45fc97
neya-mul <neyamulislam946@gmail.com>
2026-05-12 20:56:17 +0600

    modal page added

--- src/app/destinations/[id]/page.jsx
+++ src/app/destinations/[id]/page.jsx
@@ -10,7 +10,7 @@ export default async function Details({ params }) {
 
 
   return (
-    <div className='min-h-screen pt-20 flex justify-center items-center'>
+    <div className='min-h-screen border pt-20 flex justify-center items-center'>
 
       <DestinationDetailsCard destination={destination}></DestinationDetailsCard>
     </div>
--- src/components/cardComponents/DestinationDetailsCard.jsx
+++ src/components/cardComponents/DestinationDetailsCard.jsx
@@ -1,23 +1,32 @@
+import { Button } from '@heroui/react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { BiEdit } from 'react-icons/bi'
 import { IoLocationOutline } from 'react-icons/io5'
 import { MdArrowOutward } from 'react-icons/md'
+import { EditPage } from '../EditPage'
 
 export default function DestinationDetailsCard({ destination }) {
     const { _id, destinationName, country, category, price, duration, departureDate, imageUrl, description } = destination
 
     return (
-        <div className='max-w-7xl'>
+        <div className='max-w-[900px]  my-5 rounded-2xl w-full'>
+
+            <div className='flex justify-end'>
+                <EditPage></EditPage>
+            </div>
+
             <div>
                 <Image
+                    className='w-full rounded-2xl mb-10'
                     src={imageUrl}
                     alt={destinationName}
                     width={600}
-                    height={300}
+                    height={200}
                 />
             </div>
-            <div className='p-2 space-y-2'>
+            <div className='p-5 space-y-2'>
                 <p className='flex items-center'><IoLocationOutline /> {country}
                 </p>
                 <div className='flex justify-between'>
